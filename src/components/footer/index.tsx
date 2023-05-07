feabddaea8e430ccda9c721c22434ac866ac416d
卢德坤 <1364978779@qq.com>
2023-05-08 01:47:03 +0800

    init chinese

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -5,7 +5,7 @@
  * @Email: 1364978779@qq.com
  * @Date: 2023-02-12 23:38:27
  * @LastEditors: dekun lu
- * @LastEditTime: 2023-02-13 10:09:37
+ * @LastEditTime: 2023-05-08 01:46:34
  */
 
 import { defineComponent } from 'vue';
@@ -20,12 +20,11 @@ export default defineComponent({
         <div>
           <img src={logo} alt="" />
           <div>
-            <h2>What is COSELF?</h2>
+            <h2>Coself是什么？</h2>
             <p>
-              COSELF (also known as “COSELF Metaverse”) is a cross-domain trendy toy
-              brand officially established in 2021. Founded by the brand leader and designer BUZHI WU,
-              COSELF is a trendy IP brand crossing
-              over the virtual and realistic domains designed on Web 3.0 based on the concept of IP and cosmetics.
+              美妆妖精（别名“美妆精灵宇宙“）跨界潮玩品牌正式成立于2021年，由主理人&设计师吴子珺BUZHI打造，品牌以IP+美妆作为设计理念进行web3.0打造的虚实共生跨界IP潮牌。
+              美妆妖精，是基于web3.0打造的虚实共生跨界IP潮牌，目前正在积极拓展搭建元宇宙概念内容和社群搭建，通过虚拟场景，传达对未来时尚生活方式和态度的表达，围绕IP打造，例如虚拟美妆 虚拟潮玩 虚拟潮牌
+              等。作为跨界融合的探索者，美妆妖精致力于打造聚焦跨界创新，具有Z时代属性，集结新生力量的元宇宙跨界潮玩厂牌及矩阵。
             </p>
           </div>
         </div>
--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@
  * @Email: 1364978779@qq.com
  * @Date: 2023-02-12 23:20:13
  * @LastEditors: dekun lu
- * @LastEditTime: 2023-02-13 17:28:20
+ * @LastEditTime: 2023-05-08 01:36:45
  */
 // eslint-disable-next-line no-undef
 module.exports = {
@@ -14,4 +14,6 @@ module.exports = {
   },
   //相对路径
   publicPath: './',
+  //打包后的文件夹名称
+  outputDir: 'zh',
 };
